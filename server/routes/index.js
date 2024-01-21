3bc0cb86285be92e91fa4faa5ea10689e0ba2fae
Shahriar (Sara) <31452340+ShahriarKh@users.noreply.github.com>
2024-01-21 18:16:53 +0330

    fix api route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,16 @@
-module.exports = [
-  {
-    method: "GET",
-    path: "/get-types",
-    handler: "explorerController.getTypes",
-    config: {
-      policies: [],
-    },
+module.exports = {
+  "pass-data": {
+    type: "admin",
+    routes: [
+      {
+        method: "GET",
+        path: "/get-types",
+        handler: "explorerController.getTypes",
+        config: {
+          policies: [],
+          // auth: false,
+        },
+      },
+    ],
   },
-];
+};
